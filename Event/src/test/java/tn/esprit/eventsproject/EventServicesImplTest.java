d50530be07687fa0f05b056535216fc7e8714f83
Malek Labidi <60628370+maleklabidi@users.noreply.github.com>
2023-12-09 16:22:50 +0100

    Update EventServicesImplTest.java

--- Event/src/test/java/tn/esprit/eventsproject/EventServicesImplTest.java
+++ Event/src/test/java/tn/esprit/eventsproject/EventServicesImplTest.java
@@ -1,1 +1,72 @@
-dgfhdf
+package tn.esprit.eventsproject;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.eventsproject.entities.Event;
+import tn.esprit.eventsproject.entities.Logistics;
+import tn.esprit.eventsproject.entities.Tache;
+import tn.esprit.eventsproject.repositories.EventRepository;
+import tn.esprit.eventsproject.repositories.LogisticsRepository;
+import tn.esprit.eventsproject.repositories.ParticipantRepository;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+
+class EventServicesImplTest {
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @Mock
+    private ParticipantRepository participantRepository;
+
+    @Mock
+    private LogisticsRepository logisticsRepository;
+
+    @InjectMocks
+    private EventServicesImpl eventServices;
+
+    @Test
+    void testCalculCout() {
+        // Given
+        Event event = new Event();
+        event.setIdEvent(1);
+        event.setDescription("Test Event");
+        event.setDateDebut(LocalDate.now());
+        event.setDateFin(LocalDate.now().plusDays(1));
+        event.setCout(0f);
+
+        Logistics logistics = new Logistics();
+        logistics.setIdLog(1);
+        logistics.setDescription("Test Logistics");
+        logistics.setReserve(true);
+        logistics.setPrixUnit(10f);
+        logistics.setQuantite(5);
+
+        Set<Logistics> logisticsSet = new HashSet<>();
+        logisticsSet.add(logistics);
+        event.setLogistics(logisticsSet);
+
+        List<Event> events = Collections.singletonList(event);
+
+        when(eventRepository.findByParticipants_NomAndParticipants_PrenomAndParticipants_Tache(
+                "Tounsi", "Ahmed", Tache.ORGANISATEUR)).thenReturn(events);
+
+        // When
+        eventServices.calculCout();
+
+        // Then
+        verify(eventRepository, times(1)).findByParticipants_NomAndParticipants_PrenomAndParticipants_Tache(
+                "Tounsi", "Ahmed", Tache.ORGANISATEUR);
+
+        verify(eventRepository, times(1)).save(event);
+
+        // You can add additional assertions based on your specific requirements
+    }
+}
